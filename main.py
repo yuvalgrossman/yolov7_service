bbcf2aed25bfa5f200ede99c258528450f8478dc
yuvalgrossman <45980967+yuvalgrossman@users.noreply.github.com>
2023-01-30 19:23:40 +0200

    flask service

---
+++ main.py
@@ -0,0 +1,180 @@
+import argparse
+import time
+import logging
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+import sys
+sys.path.insert(0, 'yolov7')
+# from yolov7 import *
+
+from models.experimental import attempt_load
+from utils.datasets import letterbox
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from flask import Flask, request, render_template
+from flask_cors import CORS
+import numpy as np
+import base64
+import io
+from PIL import Image
+
+app = Flask(__name__)
+CORS(app)
+
+def init_detector():
+    # INIT YOLO:
+    global save_img, classify, colors, dataset, device, half, model, modelc, names, old_img_b, old_img_h, old_img_w, save_dir, save_txt, vid_path, vid_writer, view_img, webcam, imgsz, stride
+    save_img = False
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+    # Directories
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    # Initialize
+    set_logging(output_dir=save_dir.as_posix())
+    device = select_device(opt.device)
+    half = False #device.type != 'cpu'  # half precision only supported on CUDA
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    if trace:
+        model = TracedModel(model, device, imgsz)
+    if half:
+        model.half()  # to FP16
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    # Second-stage classifier
+    # classify = False
+    # if classify:
+    #     modelc = load_classifier(name='resnet101', n=2)  # initialize
+    #     modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+    # Set Dataloader
+    # vid_path, vid_writer = None, None
+    # if webcam:
+    #     view_img = check_imshow()
+    #     cudnn.benchmark = True  # set True to speed up constant image size inference
+    #     dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    # else:
+    #     dataset = LoadImages(source, img_size=imgsz, stride=stride)
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    # Warmup
+    img = torch.rand([1,3,imgsz,imgsz]).to(device)
+    if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+        old_img_b = img.shape[0]
+        old_img_h = img.shape[2]
+        old_img_w = img.shape[3]
+        for i in range(3):
+            model(img, augment=opt.augment)[0]
+
+    logging.info('object detection service ready. waiting for image')
+
+def detect_img(img0):
+    global colors, dataset, device, half, model, names, old_img_b, old_img_h, old_img_w, save_dir, save_img, save_txt, view_img
+    img = letterbox(img0, imgsz, stride=stride)[0]
+
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+
+    # YOLO SERVICE:
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Inference
+    t1 = time_synchronized()
+    with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
+        pred = model(img, augment=opt.augment)[0]
+    t2 = time_synchronized()
+    # Apply NMS
+    pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+    t3 = time_synchronized()
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+        s = ''
+        gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+            # Write results
+            for *xyxy, conf, cls in reversed(det):
+                if save_img or view_img:  # Add bbox to image
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=1)
+
+        # Print time (inference + NMS)
+        logging.info(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+        return img0, s
+
+#INIT FLASK:
+@app.route('/')
+def root():
+    init_detector()
+    return render_template('image.html', img_data='', out_msg='no image loaded')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        response = f.read()
+        img = cv2.imdecode(np.frombuffer(response, np.uint8), 1)
+
+        logging.info('uploaded image of size {}'.format(img.shape))
+        out_img, out_msg = detect_img(img)
+
+        im = Image.fromarray(out_img[:,:,::-1])
+        data = io.BytesIO()
+        im.save(data, "JPEG")
+        encoded_img_data = base64.b64encode(data.getvalue())
+        return render_template("image.html", img_data=encoded_img_data.decode('utf-8'), out_msg=out_msg)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+
+    app.run(host='localhost', port='5000', debug=True)
